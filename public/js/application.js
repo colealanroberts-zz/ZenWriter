78a291ce6ebb52501effddc83cbb9d2ce3151af5
Cole Roberts <cole@Coles-MacBook-Pro.local>
2015-05-04 14:09:54 -0500

    Continue building

--- public/js/application.js
+++ public/js/application.js
@@ -5,20 +5,51 @@
     // Dev Env Var
     var DEV = true;
 
+    // Quick Dev Log
+    function logC(cmd) {
+        if (DEV) {
+            console.log(cmd);
+        }
+    }
+
+    // Methods
+    var $el = Element.prototype;
+
+    $el.addClass = function(className) {
+        this.classList.add(className);
+    };
+
+    $el.removeClass = function(className) {
+        this.classList.remove(className);
+    };
+
+    $el.toggleClass = function(className) {
+        this.classList.toggle(className);
+    };
+
+    $el.html = function(html) {
+        this.innerHTML = html;
+    };
+
     // Path to Users Resource
     var resPath = MacGap.documentsPath;
-
-    if (DEV) { console.log(resPath); }
 
     // Elements
     var $inputTask     = document.querySelector('.js-input--task'),
         $btnCreateTask = document.querySelector('.js-btn--create-task'),
         $taskList      = document.querySelector('.js-task-list'),
-        $overlay       = document.querySelector('.js-focus-overlay'),
-        $proverbText   = document.querySelector('.js-proverb-text');
+        $overlay       = document.querySelector('.js-focus-overlay');
 
     // Saved Tasks
     var tasks = [];
+
+    function updateBadgeCount(data) {
+        // Count the number of keys in our returned obj
+        var taskCount = Object.keys(data).length;
+
+        // Set badge count
+        MacGap.Dock.addBadge(taskCount);
+    }
 
     function createTask() {
 
@@ -28,31 +59,29 @@
 
         // Push the task to the tasks Array
         tasks.push(task);
-        console.log('current tasks are: ' + tasks);
 
         // Iterate and push to HTML list
         var taskLength = tasks.length;
-        console.log(taskLength);
-
-        console.log(tasks);
 
         for (var i = 0; i < taskLength; i++) {
             var taskItem = '<li class="task__item">' + tasks[i].detail + '</li>';
         }
 
+        // Add class to the $taskList
         $taskList.innerHTML += taskItem;
         
         // Save to local list
         MacGap.File.write(resPath + '/ZenTasksList', tasks, 'json');
-        updateList();
+        
+        logC(tasks);
 
         // Clear input and hide overlay
         $inputTask.value = '';
-        $overlay.classList.remove('show');
-    }
+        $overlay.removeClass('show');
+        $btnCreateTask.removeClass('clickable');
 
-    function setProverbText(msg) {
-        $proverbText.innerHTML = msg;
+        // Update the badge
+        updateBadgeCount(tasks);
     }
 
     function notify(Title, Msg) {
@@ -62,56 +91,48 @@
         });
     }
 
-    function disableWebViewBounce() {
-        document.ontouchmove = function(event) {
-            event.preventDefault();
-        };
+    function getExistingList() {
+        var existingData = MacGap.File.read(resPath + '/ZenTasksList', 'json');
+        JSON.stringify(existingData);
+        var taskCount = Object.keys(existingData).length;
+
+        // Populate the list
+        for (var i in existingData) {
+            tasks.push(existingData[i].detail);
+            var taskItem = '<li class="task__item">' + existingData[i].detail + '</li>';
+            $taskList.innerHTML += taskItem;
+        }
+        
+        // Concat the Array
+        tasks.push(existingData);
+        updateBadgeCount(existingData);
     }
 
-    function updateList() {
+    function createNewList() {
+        // If we have no list then reset tasks array and create a new file
+        MacGap.File.write(resPath + '/ZenTasksList', tasks, 'json');
+        logC('Created a new list!');
+    }
+
+    function getAllLists() {
         var savedList = MacGap.File.exists(resPath + '/ZenTasksList');
-
         if (savedList === 0) {
-            MacGap.File.write(resPath + '/ZenTasksList', tasks, 'json');
-            console.log('Created a new list');
-        } else  if (savedList > 0) {
-            var fileDataString = MacGap.File.read(resPath + '/ZenTasksList', 'json');
-            console.log('Reading in list');
-
-            JSON.stringify(fileDataString);
-
-            // Populate array with tasks
-            tasks.push(fileDataString);
-
-            // Count the number of keys in our returned obj
-            var taskCount = Object.keys(fileDataString).length;
-
-            if (taskCount === 0) {
-                setProverbText('Zen stuff....');
-
-            } else if (taskCount > 0) {
-                
-                // Set badge count
-                MacGap.Dock.addBadge(taskCount);
-
-                // Hide proverb text
-                $proverbText.style.display = 'none';
-            }
+            createNewList();
+        } else  if (savedList === 1) {
+            getExistingList();
         }
     }
 
     function validateInput() {
 
         // Show the focus overlay
-        $overlay.classList.add('show');
+        // $overlay.addClass('show');
 
         $inputTask.addEventListener('keydown', function(e) {
 
             var key = e.which || e.keyCode;
-            console.log($inputTask.value.length);
-
-            if ($inputTask.value.length > 0) {
-                $btnCreateTask.classList.add('clickable');
+            if ($inputTask.value.length > 1) {
+                $btnCreateTask.addClass('clickable');
                 $btnCreateTask.addEventListener('click', createTask);
 
                 if (key === 13) {
@@ -119,7 +140,7 @@
                 }
 
             } else {
-                $btnCreateTask.classList.remove('clickable');
+                $btnCreateTask.removeClass('clickable');
             }
         });
     }
@@ -128,13 +149,12 @@
     notify('ZenTasks', 'App is running!');
 
     // Load in lists
-    updateList();
-    disableWebViewBounce();
+    getAllLists();
 
     // Validate our form
     $inputTask.addEventListener('focus', validateInput);
 
     $inputTask.addEventListener('blur', function() {
-        $overlay.classList.remove('show');
+        $overlay.removeClass('show');
     });
 })();
